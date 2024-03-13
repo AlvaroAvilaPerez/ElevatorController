d41b9882bb4a1fbe8b1d13c019d62af4b8748897
PaolaCanelas <canelaspaola0@gmail.com>
2024-03-13 11:10:03 -0400

    controlsistema

--- sistema/controlEdificio.py
+++ sistema/controlEdificio.py
@@ -2,6 +2,7 @@ import time
 
 
 class Edificio:
+
     def __init__(self, pisos, ascensores):
         self.pisos = pisos
         self.ascensores = ascensores
@@ -15,10 +16,8 @@ class Piso:
         self.ascensor = ascensor
         self.tiempo_de_apertura_ascensor = 5
 
-
     def llamar_ascensor(self, arriba):
         if arriba:
-            print("Subiendo:^^^^^^^")
             self.boton_arriba = True
             self.abrir_ascensor()
             self.boton_arriba = False
@@ -29,19 +28,18 @@ class Piso:
             self.boton_abajo = False
 
     def abrir_ascensor(self):
-        print("Se Abre el Ascensor")
+        print("<<<<<< ABRIR ASCENSOR >>>>>>")
         self.ascensor.abierto = True
-        time.sleep(self.tiempo_de_apertura_ascensor)
         self.ascensor.abierto = False
-        print("Se Cierra el Ascensor")
+        time.sleep(2)
 
 
 class Ascensor:
     def __init__(self, botones_de_piso):
         self.botones_de_piso = botones_de_piso
+        self.piso = 0
         self.piso_actual = 0
         self.abierto = False
-
 
     def ir_al_piso(self, numero_de_piso):
         botones = list(filter(lambda button: button.numero_de_piso == numero_de_piso, self.botones_de_piso))
@@ -52,10 +50,13 @@ class Ascensor:
         while self.piso_actual != numero_de_piso:
             if self.piso_actual < numero_de_piso:
                 self.subir_piso()
+            elif self.bajar_piso():
+                print("Piso: " + str(self.piso_actual))
+                print('----------------')
             else:
-                self.bajar_piso()
-            print("Piso: " + str(self.piso_actual))
-        print('----------------')
+                self.piso_actual != numero_de_piso
+                print(f'Error!! El piso {numero_de_piso} no existe en el edificio')
+                print('----------------')
 
     def subir_piso(self):
         self.piso_actual = self.piso_actual + 1
@@ -63,29 +64,25 @@ class Ascensor:
     def bajar_piso(self):
         self.piso_actual = self.piso_actual - 1
 
+
 class BotonDePiso:
-            def __init__(self, numero_de_piso):
-                self.numero_de_piso = numero_de_piso
-                self.encendido = False
+    def __init__(self, numero_de_piso):
+        self.numero_de_piso = numero_de_piso
+        self.encendido = False
+
 
 class Configuracion():
-    def __init__(self):
-        self.piso = 0
-        self.ascensor = None
-        self.botones_de_piso = set()
-
+    def __init__(self, numero_de_piso, ascensor):
+        self.numero_de_piso = numero_de_piso
+        self.ascensor = 1
 
     def edificio_torre(self):
-        while True:
-        edificio.ascensor = Ascensor()
-        edificio.ascensor.piso = ascensor_piso
-        edificio.botones_de_piso = set(botones_de_pisos)
-        if piso.llamar_ascensor():
-        ascensorA.ir_al_piso(piso.numero_piso)
-
-        return edificio
+        self.botones_de_pisos = [BotonDePiso(i) for i in range(self.numero_de_piso)]
+        self.ascensor = Ascensor(self.botones_de_pisos)
+        self.pisos = [Piso(i, self.ascensorA) for i in range(self.numero_de_piso + 1)]
 
 
-edificio_torre = Configuracion(3, 1)
-edificio_torre = Configuracion(4, 1)
-edificio_torre = Configuracion(7, 1)
+# edificio_torre = Configuracion(15, 1)
+edificio_torre.pisos[0].llamar_ascensor(arriba=True)
+ediificio_torre.ascensor.ir_al_piso(12)
+ediificio_torre.ascensor.ir_al_piso(1)
