eb4bc26933c1d7f97661b440db121f1f4c4c94f5
PaolaCanelas <canelaspaola0@gmail.com>
2024-03-15 14:43:36 -0400

    fix ControlAscensores

--- sistema/configuracionEdificio.py
+++ sistema/configuracionEdificio.py
@@ -2,11 +2,12 @@ from sistema.pisos import Piso, BotonDePiso
 from sistema.ascensor import Ascensor
 
 class Configuracion():
-    def __init__(self, numero_de_piso, ascensor):
+    def __init__(self, numero_de_piso, numero_de_ascensores):
         self.numero_de_piso = numero_de_piso
-        self.ascensor = ascensor
-        self.botones_de_pisos = [BotonDePiso(i) for i in range(self.numero_de_piso + 1)]
-        self.pisos = [Piso(i, self.ascensor) for i in range(self.numero_de_piso + 1)]
-        for i in range(ascensor):
-            # self.botones_de_pisos = [BotonDePiso(i) for i in range(self.numero_de_piso + 1)]
-            print(i)
+        self.numero_de_ascensores = numero_de_ascensores
+        self.ascensores = list(filter(lambda ascensor: ascensor.BotonDePiso == BotonDePiso, self.numero_de_ascensores))
+        self.botones_de_pisos_A = [BotonDePiso(i) for i in range(self.numero_de_piso + 1)]
+        self.pisos = [Piso(i, self.ascensores[0]) for i in range(numero_de_piso + 1)]
+        # enumera y nos lista la cantidad e ascensores
+        for i, ascensor in enumerate(self.ascensores):
+            print(f'Ascensor {i}')
--- sistema/edificio.py
+++ sistema/edificio.py
@@ -8,11 +8,11 @@ class Edificio:
         self.ascensores = ascensores
 
 
-edificio_torre = Configuracion(20, 4)
+edificio_torre = Configuracion(20, 3)
 edificio_torre.pisos[0].llamar_ascensor(arriba=True)
-print('------ASCENSOR "A"----------')
-edificio_torre.ascensor.ir_al_piso(25)
-edificio_torre.ascensor.ir_al_piso(2)
+print('------ASCENSOR "1"----------')
+edificio_torre.ascensor[1].ir_al_piso(25)
+edificio_torre.ascensor[2].ir_al_piso(2)
 
 # print('------ASCENSOR "B"----------')
 # edificio_torre.ascensorB.ir_al_piso(5)
