79d92b778d7b778ae964d036e83268d4fc92bb83
Ana Alanes <anaalanes23@gmail.com>
2024-03-21 17:12:37 -0400

    fixed approach ana

--- sistema/floorbutton.py
+++ sistema/floorbutton.py
@@ -1,7 +1,6 @@
-
 class FloorButton:
-    """Constructor method to initialize an object of the FloorButton class.
-            floor_number: The floor number to which the button is associated"""
     def __init__(self, floor_number: int):
+        """Constructor method to initialize an object of the FloorButton class.
+                  floor_number: The floor number to which the button is associated"""
         self.floor_number = floor_number
         self.switched_on = False
