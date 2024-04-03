931ef64d834fcf1e6a00b8497bcaff3510aabc28
Ana Alanes <anaalanes23@gmail.com>
2024-04-03 17:29:28 -0400

    ControllerElevator

--- test/test_elevator.py
+++ test/test_elevator.py
@@ -8,12 +8,14 @@ def test_go_up_floor():
     elevator_t.go_up_floor()
     assert elevator_t.current_floor == 1
 
+
 def test_lower_floor():
     floor_button = FloorButton(3)
     elevator_t = Elevator([floor_button])
     elevator_t.current_floor = 15
     elevator_t.lower_floor()
     assert elevator_t.current_floor == 14
+
 
 def test_go_to_floor():
     elevator = Elevator([FloorButton(i) for i in range(10)])
--- test/test_floors.py
+++ test/test_floors.py
@@ -1,5 +1,6 @@
 from System.floors import Floor
 
+
 def test_call_elevator():
     floor = Floor(floor_number=1, elevator=None)
     floor.call_elevator(above=False)
